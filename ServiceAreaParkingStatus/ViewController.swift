510bf17781f4174f7602592d75f582a0ebc087d2
aldalddl <aldalddl2007@gmail.com>
2023-09-19 20:28:39 +0900

    chore: Xcode 프로젝트 생성 & Code Base UI 설정

---
+++ ServiceAreaParkingStatus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServiceAreaParkingStatus
+//
+//  Created by 강민지 on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
