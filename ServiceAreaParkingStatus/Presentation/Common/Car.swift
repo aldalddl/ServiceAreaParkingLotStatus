1f101cdf3aacc13b9025c28b940831399adda743
aldalddl <aldalddl2007@gmail.com>
2023-09-25 00:52:53 +0900

    feat: 차량 종류 타입 생성

--- ServiceAreaParkingStatus/Presentation/Common/Car.swift
+++ ServiceAreaParkingStatus/Presentation/Common/Car.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+enum Car {
+    case large
+    case small
+    case disabled
+
+    var icon: UIImageView {
+        switch self {
+        case .large:
+            return UIImageView(image: UIImage(named: "largeCar"))
+        case .small:
+            return UIImageView(image: UIImage(named: "smallCar"))
+        case .disabled:
+            return UIImageView(image: UIImage(named: "disabledCar"))
+        }
+    }
+
+    var name: String {
+        switch self {
+        case .large:
+            return "대형"
+        case .small:
+            return "소형"
+        case .disabled:
+            return "장애인"
+        }
+    }
+}
+
