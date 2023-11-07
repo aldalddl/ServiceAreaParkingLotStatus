5a474a4953e26abbb8c399839e7d3a7efadbdef2
aldalddl <aldalddl2007@gmail.com>
2023-11-07 20:47:18 +0900

    feat: custom color 추가 및 ColorExtension 생성하여 color 등록

---
+++ ServiceAreaParkingStatus/Resources/ColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtension.swift
+//  ServiceAreaParkingStatus
+//
+//  Created by 강민지 on 2023/11/05.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(named: "Primary")
+    static let background = UIColor(named: "Background")
+    static let lightBackground = UIColor(named: "LightBackground")
+    static let button = UIColor(named: "Button")
+    static let searchbar = UIColor(named: "Searchbar")
+    static let secondary = UIColor(named: "Secondary")
+}
