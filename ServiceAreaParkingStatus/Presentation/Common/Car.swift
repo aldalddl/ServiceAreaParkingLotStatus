1eb8cb8f8b43113c10c40a1dd5e7dc03e18c6781
aldalddl <aldalddl2007@gmail.com>
2023-11-07 23:54:14 +0900

    refactor: Car 타입에 프로퍼티 추가

--- ServiceAreaParkingStatus/Presentation/Common/Car.swift
+++ ServiceAreaParkingStatus/Presentation/Common/Car.swift
@@ -12,12 +12,13 @@ struct Car {
     let type: String
     let remainNumber: Int?
     let totalNumber: Int?
+    let iconImageView: UIImageView
 }
 
 extension Car {
     static var list = [
-        Car(type: "대형", remainNumber: nil, totalNumber: nil),
-        Car(type: "소형", remainNumber: nil, totalNumber: nil),
-        Car(type: "장애인", remainNumber: nil, totalNumber: nil)
+        Car(type: "대형", remainNumber: nil, totalNumber: nil, iconImageView: UIImageView(image: UIImage(named: "대형"))),
+        Car(type: "소형", remainNumber: nil, totalNumber: nil, iconImageView: UIImageView(image: UIImage(named: "소형"))),
+        Car(type: "장애인 전용", remainNumber: nil, totalNumber: nil, iconImageView: UIImageView(image: UIImage(named: "장애인")))
     ]
 }
