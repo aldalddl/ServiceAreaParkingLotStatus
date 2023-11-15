251876f82d146df0e0be801e6eb0ef13094632e8
aldalddl <aldalddl2007@gmail.com>
2023-11-15 16:04:58 +0900

    fix: 서치바 배경색을 투명에서 기본배경색으로 변경

--- ServiceAreaParkingStatus/Presentation/Common/SearchBar.swift
+++ ServiceAreaParkingStatus/Presentation/Common/SearchBar.swift
@@ -20,7 +20,7 @@ class SearchBar: UISearchBar {
     }
     
     func setup() {
-        self.backgroundColor = .clear
+        self.backgroundColor = .background
         self.placeholder = "휴게소 이름을 입력하세요"
     }
 
